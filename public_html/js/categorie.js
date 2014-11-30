983eef61100967ee9453aa8cf5a67c676b134d49
bastienmaria <bastien.maria@gmail.com>
2014-11-30 21:44:32 +0100

    suppr + ou - fonctionnel update non terminé

--- public_html/js/categorie.js
+++ public_html/js/categorie.js
@@ -1,3 +1,4 @@
+var self = this;
 /*
  Ceci est equivalent à une classe Java, on veut faire propre!
  */
@@ -12,7 +13,6 @@ var Category = function (categorie) {
  Elle assure la communication entre la vue et le modèle, une sorte de pont quoi! 
  */
 var ViewModel = function (categories) {
-    var self = this;
     //représente la liste des catégories
     //La fonction prend la réponse obtenue du serveur en paramètre
     //Ici nous supposons que vous avez chargé la liste des catégories
@@ -23,10 +23,10 @@ var ViewModel = function (categories) {
 
 };
 
-self.remove = function (categorie) {
+self.removed = function (categorie) {
     self.categories.remove(categorie);
     $.ajax({
-        url: "http://localhost:8080/BiblioProject/webresources/categorie/" + self.id,
+        url: ["http://localhost:8080/BiblioProject/webresources/categorie/" + parseInt(this.id())],
         type: "DELETE",
         contentType: "application/json",
         headers: {
@@ -34,32 +34,46 @@ self.remove = function (categorie) {
         }
     })
             .success(function (data, status, jq) {
-                console.log(status);
-                self.categories.remove(categorie);
+                alert(status);
+                // self.categories.remove(categorie);
             })
             .error(function (jq, status, error) {
-                alert(status);
                 $(".error").text(JSON.stringify(status + " " + error));
-
             });
 };
-//self.update = function (categorie) {
+
+//self.updated = function (categorie) {
 //    self.categories.update(categorie);
-//    $.ajax({
-//        url: "http://localhost:8080/BiblioProject/webresources/categorie/",
+//    jQuery.ajax({
 //        type: "PUT",
-//        contentType: "application/json",
-//        headers: {
-//            Accept: "application/json"
+//        url: ["http://localhost:8080/BiblioProject/webresources/categorie/" + parseInt(this.id())],
+//        contentType: "application/json; charset=utf-8",
+//        data: categorie.toJsonString(),
+//        dataType: "json",
+//        success: function (data, status, jqXHR) {
+//            alert("ok");
+//        },
+//        error: function (jqXHR, status) {
+//            alert("error");
 //        }
-//    })
-//            .success(function (data, status, jq) {
-//                alert(status);
-//                self.categories.update(categorie);
-//            })
-//            .error(function (jq, status, error) {
-//                alert(status);
-//                $(".error").text(JSON.stringify(status + " " + error));
+//    });
 //
-//            });
 //};
+
+function updateCategorie(id, nom, description) {
+    alert(id + '  ' + nom + '   ' + description + '  ');
+    jQuery.ajax({
+        type: "PUT",
+        url: ["http://localhost:8080/BiblioProject/webresources/categorie/" + parseInt(this.id())],
+        contentType: "application/json; charset=utf-8",
+        data: categorie.toJsonString(),
+        dataType: "json",
+        success: function (data, status, jqXHR) {
+            // do something
+        },
+        error: function (jqXHR, status) {
+            // error handler
+
+        }
+    });
+}
