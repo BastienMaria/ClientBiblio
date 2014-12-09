b860bdb84bdeb04f3228def076654d96f0cbfa4a
bastienmaria <bastien.maria@gmail.com>
2014-12-09 10:13:03 +0100

    ...

--- public_html/js/ws.js
+++ public_html/js/ws.js
@@ -35,7 +35,6 @@ $(document).ready(function () {
     getData();
 });
 
-
 var getData = function () {
     $.ajax({
         url: "http://localhost:8080/BiblioProject/webresources/categorie",
@@ -44,14 +43,32 @@ var getData = function () {
             Accept: "application/json"
         }
     }).success(function (data, status, jq) {
-//Cette fonction indique à knockout d'appliquer les données aux éléments de la page 
-//Elle est toujours appelée quand les données sont pretes et est appelée qu'une fois 
-        ko.applyBindings(new ViewModel(data));
+        //Cette fonction indique à knockout d'appliquer les données aux éléments de la page 
+        //Elle est toujours appelée quand les données sont pretes et est appelée qu'une fois 
+        if (data.status)
+            ko.applyBindings(new ViewModel(data.data));
+        else {
+            alert(data.message)
+        }
     }).error(function (jq, status, error) {
         $(".error").text(JSON.stringify(status + " " + error));
 
     });
 };
+
+$.ajax({
+    url: "http://localhost:8080/bibliotheque_ntdp/webresources/category",
+    type: "GET",
+    headers: {
+        Accept: "application/json",
+        miage_authorization: "12345678903"
+
+    }
+
+}).success(function (data, status, jq) {
+
+}).error(function (jq, status, error) {
+});
 
 
 
